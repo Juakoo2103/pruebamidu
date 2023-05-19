4907e43fc0a5e8851d6e805c580c43012a4b3726
Juakoo2103 <77761177+Juakoo2103@users.noreply.github.com>
2023-05-19 01:08:19 -0400

    primer commit

---
+++ react-prueba-trainee/main.jsx
@@ -0,0 +1,8 @@
+import ReactDOM from 'react-dom/client'
+import './src/index.css'
+import { App } from './src/App'
+
+ReactDOM.createRoot(document.getElementById('app')).render(
+  
+  <App/>
+)
---
+++ react-prueba-trainee/src/App.jsx
@@ -0,0 +1,9 @@
+export function App (){
+
+    return(
+        <main>
+        <h1>App de gatitos</h1>
+        
+        </main>
+    )
+}
